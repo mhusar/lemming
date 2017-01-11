1652d1259da3f844ca2a8f4d5da6f9d04bfaeee8
Marcus Husar <marcus.husar@gmail.com>
2017-01-11 16:33:08 +0100

    Add a HomePage class and WebApplication class to enable the wicket backend.

---
+++ src/main/java/lemming/api/HomePage.java
@@ -0,0 +1,46 @@
+package lemming.api;
+
+import lemming.api.auth.WebSession;
+import lemming.api.character.CharacterEditPage;
+import lemming.api.lemma.LemmaIndexPage;
+import lemming.api.ui.UserBookmarkablePageLink;
+import lemming.api.ui.page.BasePage;
+import lemming.api.user.UserEditPage;
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
+import java.util.logging.Logger;
+
+/**
+ * The home or index page of the application.
+ */
+@AuthorizeInstantiation("SIGNED_IN")
+public class HomePage extends BasePage {
+    /**
+     * Determines if a deserialized file is compatible with this class.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * A logger named corresponding to this class.
+     */
+    private static final Logger logger = Logger.getLogger(HomePage.class.getName());
+
+    /**
+     * Initializes a home page.
+     */
+    public HomePage() {
+        BookmarkablePageLink<Void> lemmaIndexLink = new BookmarkablePageLink<Void>("lemmaIndexLink",
+                LemmaIndexPage.class);
+        BookmarkablePageLink<Void> userEditLink = new BookmarkablePageLink<Void>("userEditLink", UserEditPage.class);
+        UserBookmarkablePageLink characterEditPageLink = new UserBookmarkablePageLink("characterEditPageLink",
+                CharacterEditPage.class);
+
+        // check if the session is expired
+        WebSession.get().checkSessionExpired(getPageClass());
+
+        add(lemmaIndexLink);
+        add(userEditLink);
+        add(characterEditPageLink);
+    }
+}
