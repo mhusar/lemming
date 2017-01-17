d1e59638489dff1637e835c2ff51d4769bfa17a5
Marcus Husar <marcus.husar@gmail.com>
2017-01-17 16:21:12 +0100

    Link to page ContextIndexPage from HomePage and HeaderPanel.

--- src/main/java/lemming/api/HomePage.java
+++ src/main/java/lemming/api/HomePage.java
@@ -2,6 +2,7 @@ package lemming.api;
 
 import lemming.api.auth.WebSession;
 import lemming.api.character.CharacterEditPage;
+import lemming.api.context.ContextIndexPage;
 import lemming.api.lemma.LemmaIndexPage;
 import lemming.api.pos.PosIndexPage;
 import lemming.api.ui.UserBookmarkablePageLink;
@@ -31,6 +32,8 @@ public class HomePage extends BasePage {
      * Initializes a home page.
      */
     public HomePage() {
+        BookmarkablePageLink<Void> contextIndexLink = new BookmarkablePageLink<Void>("contextIndexLink",
+                ContextIndexPage.class);
         BookmarkablePageLink<Void> lemmaIndexLink = new BookmarkablePageLink<Void>("lemmaIndexLink",
                 LemmaIndexPage.class);
         BookmarkablePageLink<Void> posIndexLink = new BookmarkablePageLink<Void>("posIndexLink", PosIndexPage.class);
@@ -41,6 +44,7 @@ public class HomePage extends BasePage {
         // check if the session is expired
         WebSession.get().checkSessionExpired(getPageClass());
 
+        add(contextIndexLink);
         add(lemmaIndexLink);
         add(posIndexLink);
         add(userEditLink);
