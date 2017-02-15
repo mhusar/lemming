0266d2b3b7b8580c2e1c3116db4f70700133ec5c
Marcus Husar <marcus.husar@gmail.com>
2017-02-15 15:52:36 +0100

    Don’t allow public access to page ResourcePage.

--- src/main/java/lemming/resource/ResourcePage.java
+++ src/main/java/lemming/resource/ResourcePage.java
@@ -1,11 +1,13 @@
 package lemming.resource;
 
 import lemming.ui.page.BasePage;
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.form.Button;
 
 /**
  * A download page for data resources.
  */
+@AuthorizeInstantiation({ "SIGNED_IN" })
 public class ResourcePage extends BasePage {
     /**
      * Creates a resource page.
