d5875eba7f4ce92d33b2931d0e9e377d2a01773c
Marcus Husar <marcus.husar@gmail.com>
2017-02-21 14:20:18 +0100

    Move javaScript code to feedback panel. Remove apostroph javascript code.

--- src/main/java/lemming/ui/panel/FeedbackPanel.java
+++ src/main/java/lemming/ui/panel/FeedbackPanel.java
@@ -2,12 +2,13 @@ package lemming.ui.panel;
 
 import org.apache.wicket.feedback.FeedbackMessage;
 import org.apache.wicket.feedback.IFeedbackMessageFilter;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 
 /**
  * A panel that displays feedback messages.
  */
-public class FeedbackPanel extends
-        org.apache.wicket.markup.html.panel.FeedbackPanel {
+public class FeedbackPanel extends org.apache.wicket.markup.html.panel.FeedbackPanel {
     /**
      * Determines if a deserialized file is compatible with this class.
      */
@@ -16,30 +17,28 @@ public class FeedbackPanel extends
     /**
      * Creates a feedback panel.
      * 
-     * @param id
-     *            ID of a feedback panel.
+     * @param id ID of a feedback panel
      */
     public FeedbackPanel(String id) {
         super(id);
+        setMarkupId(getId());
     }
 
     /**
      * Creates a feedback panel.
      * 
-     * @param id
-     *            ID of a feedback panel.
-     * @param filter
-     *            a filter for feedback messages
+     * @param id ID of a feedback panel
+     * @param filter a filter for feedback messages
      */
     public FeedbackPanel(String id, IFeedbackMessageFilter filter) {
         super(id, filter);
+        setMarkupId(getId());
     }
 
     /**
      * Returns a CSS class string.
      * 
-     * @param message
-     *            the analyzed feedback message
+     * @param message the analyzed feedback message
      * @return A CSS class string based on message level.
      */
     @Override
@@ -61,5 +60,18 @@ public class FeedbackPanel extends
         }
 
         return css;
+    }
+
+    /**
+     * Renders to the web response what the component wants to contribute.
+     *
+     * @param response response object
+     */
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        String javaScript = "jQuery('#" + getMarkupId() + " li').each(function (index) { " +
+                "var elementClass = jQuery(this).attr('class'); " +
+                "jQuery(this).removeClass(elementClass).addClass('alert alert-' + elementClass); });";
+        response.render(OnDomReadyHeaderItem.forScript(javaScript));
     }
 }
--- src/main/resources/lemming/ui/page/scripts/global.js
+++ src/main/resources/lemming/ui/page/scripts/global.js
@@ -2,9 +2,30 @@ jQuery.ajaxSetup({
     cache : false
 });
 
+function changeFormTabOrder() {
+    jQuery(".basePage form").on("keypress", function (event) {
+        var tabulatorPressed = (event.keyCode === 9) ? true : false;
+        var shiftPressed = event.shiftKey;
+
+        if (tabulatorPressed && jQuery(event.target).is(":input")) {
+            var formDescendants = jQuery(event.target).closest("form").find("*");
+            var formInputs = formDescendants.filter(":input:visible").not(":disabled").not(":button").not(":submit");
+            var firstInput = formInputs.first();
+            var lastInput = formInputs.last();
+
+            if (firstInput.is(event.target) && shiftPressed) {
+                lastInput.focus();
+                event.preventDefault();
+            } else if (lastInput.is(event.target) && !(shiftPressed)) {
+                firstInput.focus();
+                event.preventDefault();
+            }
+        }
+    });
+}
+
 jQuery(document).ready(function () {
     changeFormTabOrder();
-    setupFeedbackPanel();
 });
 
 jQuery.fn.isInViewport = function (offset) {
@@ -30,66 +51,3 @@ jQuery(window).on("resize scroll", function () {
         jQuery("input[autofocus]").first().focus();
     }
 });
-
-function changeFormTabOrder() {
-    jQuery(".basePage form").on("keypress", function (event) {
-        var tabulatorPressed = (event.keyCode === 9) ? true : false;
-        var shiftPressed = event.shiftKey;
-
-        if (tabulatorPressed && jQuery(event.target).is(":input")) {
-            var formDescendants = jQuery(event.target).closest("form").find("*");
-            var formInputs = formDescendants.filter(":input:visible").not(":disabled").not(":button").not(":submit");
-            var firstInput = formInputs.first();
-            var lastInput = formInputs.last();
-
-            if (firstInput.is(event.target) && shiftPressed) {
-                lastInput.focus();
-                event.preventDefault();
-            } else if (lastInput.is(event.target) && !(shiftPressed)) {
-                firstInput.focus();
-                event.preventDefault();
-            }
-        }
-    });
-}
-
-function setupFeedbackPanel(id) {
-    var listItems;
-
-    if (typeof id !== "undefined") {
-        listItems = jQuery(id + " li");
-    } else {
-        listItems = jQuery(".feedbackPanel li");
-    }
-
-    listItems.each(function (index) {
-        var listElementClass = jQuery(this).attr("class");
-        jQuery(this).removeClass(listElementClass).addClass("alert alert-" + listElementClass);
-    });
-}
-
-function insertCharacter(formElement, character, selectionStart, selectionEnd) {
-    var value = formElement.val();
-
-    if (selectionStart <= selectionEnd) {
-        formElement.val(value.substring(0, selectionStart) + character + value.substring(selectionEnd));
-        formElement[0].setSelectionRange(selectionStart + 1, selectionStart + 1);
-    } else {
-        formElement.val(value.substring(0, selectionEnd) + character + value.substring(selectionStart));
-        formElement[0].setSelectionRange(selectionEnd + 1, selectionEnd + 1);
-    }
-}
-
-function setupApostropheKey() {
-    jQuery(":input:text, textarea").keydown(function (event) {
-        var character = "’", formElement = jQuery(this), selectionStart, selectionEnd;
-
-        if (event.shiftKey && event.which === 163) {
-            event.preventDefault();
-            selectionStart = formElement[0].selectionStart;
-            selectionEnd = formElement[0].selectionEnd;
-
-            insertCharacter(formElement, character, selectionStart, selectionEnd);
-        }
-    });
-}
