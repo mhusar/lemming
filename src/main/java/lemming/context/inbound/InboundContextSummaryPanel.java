d85be53eb1822be3367540db1a9967cd5a8190b8
Marcus Husar <marcus.husar@gmail.com>
2017-03-21 10:43:06 +0100

    Make lots of adjustments for class InboundContextSummaryPanel and its markup.
     * Display user and date/time in a popover widget
     * Show begin and end location formatted in list item text
     * Add buttons to discard and approve inbound contexts (not active)

--- src/main/java/lemming/context/inbound/InboundContextSummaryPanel.java
+++ src/main/java/lemming/context/inbound/InboundContextSummaryPanel.java
@@ -2,8 +2,13 @@ package lemming.context.inbound;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.RefreshingView;
@@ -43,8 +48,6 @@ public class InboundContextSummaryPanel extends Panel {
         super(id);
         placeholder = new WebMarkupContainer("placeholder");
         summaryView = new InboundContextSummaryView("inboundContextSummaryView");
-
-        placeholder.add(new Label("label", getString("InboundContextSummaryPanel.placeholder")));
         add(placeholder);
         add(summaryView);
     }
@@ -88,6 +91,28 @@ public class InboundContextSummaryPanel extends Panel {
         }
 
         /**
+         * Creates a location string based on begin and end location.
+         *
+         * @param beginLocation begin location
+         * @param endLocation end location
+         * @return A location string.
+         */
+        private String createLocationString(String beginLocation, String endLocation) {
+            String[] beginStrings = beginLocation.split("_"), endStrings = endLocation.split("_");
+            String beginDocument = beginStrings[0], endDocument = endStrings[0],
+                    beginRubric = beginStrings[1].replaceFirst("^0+", ""),
+                    endRubric = endStrings[1].replaceFirst("^0+", "");
+
+            if (beginDocument.equals(endDocument)) {
+                return new StringResourceModel("InboundContextSummaryView.location1", this)
+                        .setParameters(beginDocument, beginRubric, endRubric).getString();
+            } else {
+                return new StringResourceModel("InboundContextSummaryView.location2", this)
+                        .setParameters(beginDocument, beginRubric, endDocument, endRubric).getString();
+            }
+        }
+
+        /**
          * Populates the given item container.
          *
          * @param item item which is populated
@@ -98,16 +123,87 @@ public class InboundContextSummaryPanel extends Panel {
             Instant instant = summary.getTimestamp().toInstant();
             ZonedDateTime dateTime = ZonedDateTime.ofInstant(instant, ZoneId.of("Europe/London"));
 
-            String numberOfContexts = String.valueOf(summary.getNumberOfContexts());
-            String localizedTime = dateTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
-                    .withLocale(getLocale()));
-            String localizedDate = dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)
-                    .withLocale(getLocale()));
-            String userString = summary.getUserString();
-            StringResourceModel stringResourceModel = new StringResourceModel("InboundContextSummaryView.label", this)
-                    .setParameters(numberOfContexts, userString, localizedTime, localizedDate);
+            String numberOfContexts = String.valueOf(summary.getNumberOfContexts()),
+                    locationString = createLocationString(summary.getBeginLocation(), summary.getEndLocation()),
+                    userString = summary.getUserString(),
+                    localizedDate = dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)
+                            .withLocale(getLocale())),
+                    localizedTime = dateTime.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
+                            .withLocale(getLocale()));
 
-            item.add(new Label("label", stringResourceModel.getString()));
+            String itemLabel = new StringResourceModel("InboundContextSummaryView.itemLabel", this)
+                    .setParameters(numberOfContexts, locationString).getString();
+            String popoverContent = new StringResourceModel("InboundContextSummaryView.popoverContent", this)
+                    .setParameters("<dl class='dl-horizontal'><dt>", "</dt><dd>" + userString + "</dd><dt>",
+                            "</dt><dd>" + localizedDate + "</dd><dt>", "</dt><dd>" + localizedTime + "</dd><dt>")
+                    .getString();
+
+            item.add(new Label("itemLabel", itemLabel));
+            item.add(new WebMarkupContainer("detailsButton").add(AttributeModifier.append("data-content",
+                    popoverContent)));
+            item.add(new ApproveButton("approveButton", item.getModel()));
+            item.add(new DiscardButton("discardButton", item.getModel()));
+        }
+
+        /**
+         * Renders a JavaScript header item to the web response.
+         *
+         * @param response the response object
+         */
+        @Override
+        public void renderHead(IHeaderResponse response) {
+            super.renderHead(response);
+            // initialize popovers
+            String javaScript = "jQuery(\"[data-toggle='popover']\").popover(" +
+                    "{ container: 'body', html: true, trigger: 'manual' })" +
+                    ".click(function () { jQuery(this).popover('toggle'); })" +
+                    ".blur(function () { jQuery(this).popover('hide'); });";
+            response.render(OnDomReadyHeaderItem.forScript(javaScript));
+        }
+    }
+
+    /**
+     * A button which sends the user to a page where contexts are approved.
+     */
+    private class ApproveButton extends Link<InboundContextSummary> {
+        /**
+         * Creates an approve button.
+         */
+        public ApproveButton(String id, IModel<InboundContextSummary> model) {
+            super(id, model);
+        }
+
+        /**
+         * Called on button click.
+         */
+        @Override
+        public void onClick() {
+        }
+    }
+
+    /**
+     * A but which discards inbound contexts matching inbound context summary.
+     */
+    private class DiscardButton extends AjaxLink<InboundContextSummary> {
+        /**
+         * Creates a discard button.
+         *
+         * @param id ID of the button
+         * @param model model of the button
+         */
+        public DiscardButton(String id, IModel<InboundContextSummary> model) {
+            super(id, model);
+        }
+
+        /**
+         * Called on button click.
+         *
+         * @param target target that produces an Ajax response
+         */
+        @Override
+        public void onClick(AjaxRequestTarget target) {
+            new InboundContextDao().removeBySummary(getModelObject());
+            target.add(InboundContextSummaryPanel.this);
         }
     }
 }
