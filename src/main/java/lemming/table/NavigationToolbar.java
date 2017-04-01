a63926adc77ba84cacf30f365b66081b288c96c4
Marcus Husar <marcus.husar@gmail.com>
2017-04-01 02:30:49 +0200

    Add a navigator form to data tables to allow to go to a certain page.

--- src/main/java/lemming/table/NavigationToolbar.java
+++ src/main/java/lemming/table/NavigationToolbar.java
@@ -1,11 +1,16 @@
 package lemming.table;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.extensions.ajax.markup.html.repeater.data.table.AjaxNavigationToolbar;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import java.util.Iterator;
 
 /**
  * An extended AjaxNavigationToolbar with a custom paging navigator.
- * 
+ *
  * @param <T>
  *            data type that is navigated
  * @see PagingNavigator
@@ -17,18 +22,42 @@ public class NavigationToolbar<T> extends AjaxNavigationToolbar {
     private static final long serialVersionUID = 1L;
 
     /**
+     * A navigation form with a go to page field.
+     */
+    private Panel navigatorFormPanel;
+
+    /**
      * Creates a navigation toolbar.
-     * 
+     *
      * @param table
      *            table a toolbar is attached to
      */
     public NavigationToolbar(DataTable<T, String> table) {
         super(table);
+        navigatorFormPanel = new PagingNavigatorFormPanel<T>("navigatorFormPanel", table);
+    }
+
+    /**
+     * Called when a AjaxNavigationToolbar is initialized.
+     */
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        Iterator<Component> iterator = iterator();
+
+        while (iterator.hasNext()) {
+            Component component = iterator.next();
+
+            if (component.getId().equals("span")) {
+                WebMarkupContainer spanContainer = (WebMarkupContainer) component;
+                spanContainer.add(navigatorFormPanel);
+            }
+        }
     }
 
     /**
      * Creates a paging navigator.
-     * 
+     *
      * @param navigatorId
      * @param table
      *            table a paging navigator is attached to
---
+++ src/main/java/lemming/table/PagingNavigatorFormPanel.java
@@ -0,0 +1,103 @@
+package lemming.table;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+/**
+ * A panel with a paging navigator form which allows to directly jump to pages.
+ *
+ * @param <T> class type
+ */
+public class PagingNavigatorFormPanel<T> extends Panel {
+    /**
+     * Creates a paging navigator form panel.
+     *
+     * @param id ID of the panel
+     * @param table the parent table
+     */
+    public PagingNavigatorFormPanel(String id, DataTable<T, String> table) {
+        super(id);
+        add(new PagingNavigatorForm("navigatorForm", table));
+    }
+
+    /**
+     * A form which allows to directly jump to pages.
+     */
+    private class PagingNavigatorForm extends Form<Void> {
+        /**
+         * Creates a paging navigator form.
+         *
+         * @param id ID of the form
+         * @param table the parent table
+         */
+        public PagingNavigatorForm(String id, DataTable<T, String> table) {
+            super(id);
+            TextField<String> pageTextField = new TextField<String>("page", Model.of(""));
+            AjaxButton gotoButton = new GotoButton("gotoButton", table, pageTextField);
+            add(pageTextField.setOutputMarkupId(true));
+            add(gotoButton.setOutputMarkupId(true));
+            setDefaultButton(gotoButton);
+        }
+    }
+
+    /**
+     * An Ajax button which sends the user to a different page.
+     */
+    private class GotoButton extends AjaxButton {
+        /**
+         * The parent table.
+         */
+        private DataTable<T, String> table;
+
+        /**
+         * A text field for page numbers.
+         */
+        private TextField<String> pageTextField;
+
+        /**
+         * Creates a go to button.
+         *
+         * @param id ID of the button
+         * @param table the parent table
+         * @param pageTextField a text field for page numbers
+         */
+        public GotoButton(String id, DataTable<T, String> table, TextField<String> pageTextField) {
+            super(id);
+            this.table = table;
+            this.pageTextField = pageTextField;
+        }
+
+        /**
+         * Called on form submit.
+         *
+         * @param target target that produces an Ajax response
+         * @param form the parent form
+         */
+        @Override
+        protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+            String pageString = pageTextField.getModelObject();
+            Long page = null;
+
+            if (pageString != null && pageString.matches("\\d+")) {
+                // data table pages are 0-based
+                page = Long.valueOf(pageString) - 1L;
+                page = (page < 0L) ? 0L : page;
+                page = (page >= table.getPageCount()) ? table.getPageCount() - 1L : page;
+            }
+
+            if (page != null) {
+                table.setCurrentPage(page);
+                target.add(table);
+            }
+
+            pageTextField.setModelObject("");
+            target.add(pageTextField);
+            target.add(this);
+        }
+    }
+}
