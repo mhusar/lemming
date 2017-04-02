e1ea32f111708fcccc5a6e4e9bbb7d05804595df
Marcus Husar <marcus.husar@gmail.com>
2017-04-03 01:27:43 +0200

    Don’t change form tab order per JavaScript method in global.js.
    This prevented tabindex from working properly.

--- src/main/resources/lemming/ui/page/scripts/global.js
+++ src/main/resources/lemming/ui/page/scripts/global.js
@@ -2,32 +2,6 @@ jQuery.ajaxSetup({
     cache : false
 });
 
-function changeFormTabOrder() {
-    jQuery(".basePage form").on("keypress", function (event) {
-        var tabulatorPressed = (event.keyCode === 9) ? true : false;
-        var shiftPressed = event.shiftKey;
-
-        if (tabulatorPressed && jQuery(event.target).is(":input")) {
-            var formDescendants = jQuery(event.target).closest("form").find("*");
-            var formInputs = formDescendants.filter(":input:visible").not(":disabled").not(":button").not(":submit");
-            var firstInput = formInputs.first();
-            var lastInput = formInputs.last();
-
-            if (firstInput.is(event.target) && shiftPressed) {
-                lastInput.focus();
-                event.preventDefault();
-            } else if (lastInput.is(event.target) && !(shiftPressed)) {
-                firstInput.focus();
-                event.preventDefault();
-            }
-        }
-    });
-}
-
-jQuery(document).ready(function () {
-    changeFormTabOrder();
-});
-
 jQuery.fn.isInViewport = function (offset) {
     var elementTop, elementBottom, viewportTop, viewportBottom;
 
