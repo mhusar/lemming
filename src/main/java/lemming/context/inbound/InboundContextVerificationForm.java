c2a2cf280c054adb9998a927d9a5411f4aa0fe90
Marcus Husar <marcus.husar@gmail.com>
2019-03-21 02:18:01 +0100

    Add a repeating view with context tree panels to the verification form.

--- src/main/java/lemming/context/inbound/InboundContextVerificationForm.java
+++ src/main/java/lemming/context/inbound/InboundContextVerificationForm.java
@@ -4,6 +4,7 @@ import lemming.context.Context;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.IModel;
 
 import javax.ws.rs.core.MultivaluedMap;
@@ -14,6 +15,9 @@ import java.util.List;
  * A form for the verification of inbound contexts.
  */
 class InboundContextVerificationForm extends Form<InboundContextPackage> {
+
+    RepeatingView repeatingView;
+
     /**
      * Creates a context verify form.
      *
@@ -22,46 +26,107 @@ class InboundContextVerificationForm extends Form<InboundContextPackage> {
     public InboundContextVerificationForm(IModel<InboundContextPackage> model) {
         super("InboundContextVerificationForm", model);
         add(new SubmitButton(this));
+
+        repeatingView = new RepeatingView("repeater");
+        add(repeatingView);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        InboundContextPackage contextPackage = getModelObject();
+
+        matchContextsByHash();
+        List<String> unmatchedLocations = findUnmatchedLocations(contextPackage);
+
+        for (String location : unmatchedLocations) {
+            MultivaluedMap<Integer, InboundContext> groupedContexts = new InboundContextPackageDao()
+                    .groupUnmatchedContexts(contextPackage, location);
+
+            for (Integer key : groupedContexts.keySet()) {
+                List<InboundContext> inboundContexts = groupedContexts.get(key);
+                List<Context> complements = new InboundContextDao().findComplements(inboundContexts);
+
+                // TODO: what now?
+                if (complements != null) {
+                    List<Triple> matchingTriples = computeMatchingTriples(inboundContexts, complements);
+                    ContextTreeProvider provider = new ContextTreeProvider(matchingTriples);
+                    List<InboundContext> contextsWithoutComplement = getContextsWithoutComplement(inboundContexts,
+                            matchingTriples);
+                    List<Context> complementsWithoutContext = getComplementsWithoutContext(complements,
+                            matchingTriples);
+
+                    repeatingView.add(new ContextTreePanel(repeatingView.newChildId(), provider,
+                            complementsWithoutContext, contextsWithoutComplement));
+                }
+            }
+        }
+
+        // TODO: show something else
+        if (repeatingView.size() == 0) {
+            repeatingView.setVisible(false);
+        }
+    }
+
+    /**
+     * Match contexts by hash.
+     */
+    private void matchContextsByHash() {
+        InboundContextPackage contextPackage = getModelObject();
+        new InboundContextPackageDao().matchContextsByHash(contextPackage);
+    }
+
+    /**
+     * Finds and sets locations with unmatched contexts.
+     *
+     * @param contextPackage package of inbound contexts
+     */
+    private List<String> findUnmatchedLocations(InboundContextPackage contextPackage) {
+        return new InboundContextPackageDao().findUnmatchedContextLocations(contextPackage);
+    }
+
+    /**
+     * Computes matching triples for a list of contexts.
+     *
+     * @param contexts a list of inbound contexts
+     * @param complements a list of context complements
+     * @return A list of matching triples.
+     */
+    private List<Triple> computeMatchingTriples(List<InboundContext> contexts, List<Context> complements) {
+        if (complements != null) {
+            MultivaluedMap<Integer, Triple> distanceMap = MatchHelper.getTriples(complements, contexts);
+            return MatchHelper.computeMatchingTriples(distanceMap);
+        }
+
+        return new ArrayList<>();
+    }
+
+    private List<InboundContext> getContextsWithoutComplement(List<InboundContext> inboundContexts,
+                                                              List<Triple> matchingTriples) {
+        List<InboundContext> unmatchedInboundContexts = new ArrayList<>(inboundContexts);
+
+        for (Triple triple : matchingTriples) {
+            unmatchedInboundContexts.remove(triple.getInboundContext());
+        }
+
+        return unmatchedInboundContexts;
+    }
+
+    private List<Context> getComplementsWithoutContext(List<Context> complements,
+                                                       List<Triple> matchingTriples) {
+        List<Context> unmatchedComplements = new ArrayList<>(complements);
+
+        for (Triple triple : matchingTriples) {
+            unmatchedComplements.remove(triple.getContext());
+        }
+
+        return unmatchedComplements;
     }
 
     private class SubmitButton extends AjaxButton {
 
-        public SubmitButton(Form<InboundContextPackage> form) {
+        SubmitButton(Form<InboundContextPackage> form) {
             super("submitButton", form);
-        }
-
-        /**
-         * Match contexts by hash.
-         */
-        private void matchContextsByHash() {
-            InboundContextPackage contextPackage = (InboundContextPackage) getForm().getModelObject();
-            new InboundContextPackageDao().matchContextsByHash(contextPackage);
-        }
-
-        /**
-         * Finds and sets locations with unmatched contexts.
-         *
-         * @param contextPackage package of inbound contexts
-         */
-        private List<String> findUnmatchedLocations(InboundContextPackage contextPackage) {
-            return new InboundContextPackageDao().findUnmatchedContextLocations(contextPackage);
-        }
-
-        /**
-         * Computes matching triples for a list of contexts.
-         *
-         * @param contexts a list of contexts
-         * @return A list of matching triples.
-         */
-        private List<Triple> computeMatchingTriplets(List<InboundContext> contexts) {
-            List<Context> complements = new InboundContextDao().findComplements(contexts);
-
-            if (complements != null) {
-                MultivaluedMap<Integer, Triple> tripleMap = MatchHelper.getTriples(complements, contexts);
-                return MatchHelper.computeMatchingTriples(tripleMap);
-            }
-
-            return new ArrayList<>();
         }
 
         /**
@@ -73,20 +138,6 @@ class InboundContextVerificationForm extends Form<InboundContextPackage> {
         @Override
         protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
             super.onSubmit(target, form);
-            InboundContextPackage contextPackage = (InboundContextPackage) getForm().getModelObject();
-
-            matchContextsByHash();
-            List<String> unmatchedLocations = findUnmatchedLocations(contextPackage);
-
-            for (String location : unmatchedLocations) {
-                MultivaluedMap<Integer, InboundContext> groupedContexts = new InboundContextPackageDao()
-                        .groupUnmatchedContexts(contextPackage, location);
-
-                for (Integer key : groupedContexts.keySet()) {
-                    List<InboundContext> contexts = groupedContexts.get(key);
-                    List<Triple> matchingTriples = computeMatchingTriplets(contexts);
-                }
-            }
         }
     }
 }
