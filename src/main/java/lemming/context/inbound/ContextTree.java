f87bcb99f5f1a0ac21df7a8ca14fab298c368c0d
Marcus Husar <marcus.husar@gmail.com>
2019-03-27 10:40:09 +0100

    Change the style class for root nodes depending on their children.

--- src/main/java/lemming/context/inbound/ContextTree.java
+++ src/main/java/lemming/context/inbound/ContextTree.java
@@ -10,7 +10,9 @@ import org.apache.wicket.extensions.markup.html.repeater.tree.content.Folder;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * A tree used to map inbound contexts to contexts.
@@ -112,6 +114,41 @@ public class ContextTree extends NestedTree<BaseContext> {
             MarkupContainer container = super.newLinkComponent(id, model);
             container.setEscapeModelStrings(false);
             return container;
+        }
+
+        /**
+         * Returns a style class for the folder.
+         *
+         * @return A style class.
+         */
+        @Override
+        protected String getStyleClass() {
+            String styleClass = super.getStyleClass();
+            ITreeProvider<BaseContext> provider = ContextTree.this.getProvider();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            if (getModelObject() instanceof InboundContext) {
+                return styleClass;
+            }
+
+            if (provider.hasChildren(getModelObject())) {
+                List<BaseContext> children = new ArrayList<>();
+                Iterator<? extends BaseContext> iterator = provider.getChildren(getModelObject());
+
+                while (iterator.hasNext()) {
+                    children.add(iterator.next());
+                }
+
+                if (children.size() > 1) {
+                    return stringBuilder.append(styleClass).append(" ").append("has-multiple-children").toString();
+                } else if (getModelObject().getKeyword().equals(children.get(0).getKeyword())) {
+                    return styleClass;
+                } else {
+                    return stringBuilder.append(styleClass).append(" ").append("has-different-child").toString();
+                }
+            } else {
+                return stringBuilder.append(styleClass).append(" ").append("has-no-children").toString();
+            }
         }
     }
 }
