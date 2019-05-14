ed3b5d5fe5630cd53c13f83fab086283290b8817
Marcus Husar <marcus.husar@gmail.com>
2019-05-14 15:37:54 +0200

    Use a list container as drop target. Use a sortable provider as list provider.

--- src/main/java/lemming/context/inbound/ContextTreePanel.java
+++ src/main/java/lemming/context/inbound/ContextTreePanel.java
@@ -5,15 +5,24 @@ import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import wicketdnd.*;
 import wicketdnd.theme.WindowsTheme;
 
+import javax.xml.crypto.Data;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -33,7 +42,8 @@ public class ContextTreePanel extends Panel {
         super(id);
         ContextTree tree = new ContextTree(provider);
         final WebMarkupContainer listContainer = new WebMarkupContainer("listContainer");
-        ListView<InboundContext> listView = new InboundContextListView(inboundContexts);
+        final WebMarkupContainer listDropTarget = new WebMarkupContainer("listDropTarget");
+        DataView<InboundContext> dataView = new InboundContextDataView(inboundContexts);
 
         tree.add(AttributeModifier.append("class", "tree tree-theme-windows"));
         tree.setOutputMarkupId(true);
@@ -42,9 +52,12 @@ public class ContextTreePanel extends Panel {
         tree.add(new TreeDropTarget(tree).dropCenter(".tree-content.context-droppable"));
 
         listContainer.add(new WindowsTheme()); // from wicketdnd
-        listContainer.add(new ListDragSource(listContainer, listView).drag("tbody tr"));
+        listContainer.add(new ListDragSource(listContainer, dataView).drag("tbody tr"));
+        listContainer.add(new ListDropTarget(listContainer, dataView).dropCenter("table"));
         listContainer.setOutputMarkupId(true);
-        listContainer.add(listView);
+        listContainer.add(listDropTarget);
+        listDropTarget.setOutputMarkupId(true);
+        listDropTarget.add(dataView);
 
         add(new Label("heading", location));
         add(new CollapseAllButton(tree));
@@ -118,26 +131,27 @@ public class ContextTreePanel extends Panel {
     /**
      * A list view for inbound contexts.
      */
-    private class InboundContextListView extends ListView<InboundContext> {
+    private class InboundContextDataView extends DataView<InboundContext> {
         /**
-         * Creates a list view.
+         * Creates a data view.
          *
          * @param contexts inbound contexts
          */
-        public InboundContextListView(List<InboundContext> contexts) {
-            super("inboundContextListView", contexts);
+        public InboundContextDataView(List<InboundContext> contexts) {
+            super("inboundContextDataView", new InboundContextDataProvider(contexts));
         }
 
         /**
          * Creates an item.
          *
+         * @param id if of item
          * @param index index of item
-         * @param itemModel model of item
+         * @param model model of item
          * @return The item.
          */
         @Override
-        protected ListItem<InboundContext> newItem(int index, IModel<InboundContext> itemModel) {
-            ListItem<InboundContext> item = super.newItem(index, itemModel);
+        protected Item<InboundContext> newItem(String id, int index, IModel<InboundContext> model) {
+            Item<InboundContext> item =  super.newItem(id, index, model);
             item.setOutputMarkupId(true);
             return item;
         }
@@ -145,13 +159,57 @@ public class ContextTreePanel extends Panel {
         /**
          * Populates an item
          *
-         * @param listItem the item
+         * @param item the item
          */
         @Override
-        protected void populateItem(ListItem<InboundContext> listItem) {
-            InboundContext context = listItem.getModelObject();
-            listItem.add(new Label("number", context.getNumber()));
-            listItem.add(new Label("keyword", context.getKeyword()));
+        protected void populateItem(Item<InboundContext> item) {
+            InboundContext context = item.getModelObject();
+            item.add(new Label("number", context.getNumber()));
+            item.add(new Label("keyword", context.getKeyword()));
+        }
+    }
+
+    private class InboundContextDataProvider implements IDataProvider<InboundContext> {
+
+        private List<InboundContext> contexts;
+
+        InboundContextDataProvider(List<InboundContext> contexts) {
+            contexts.sort((context1, context2) -> context1.getNumber().compareTo(context2.getNumber()));
+            this.contexts = contexts;
+        }
+
+        public void add(InboundContext context) {
+            contexts.add(context);
+            contexts.sort((context1, context2) -> context1.getNumber().compareTo(context2.getNumber()));
+        }
+
+        public void remove(InboundContext context) {
+            contexts.remove(context);
+        }
+
+        public boolean contains(InboundContext context) {
+            return contexts.contains(context);
+        }
+
+        @Override
+        public Iterator<? extends InboundContext> iterator(long first, long count) {
+            int fromIndex = (int) first;
+            int toIndex = (int) (first + count);
+            return contexts.subList(fromIndex, toIndex).iterator();
+        }
+
+        @Override
+        public long size() {
+            return contexts.size();
+        }
+
+        @Override
+        public IModel<InboundContext> model(InboundContext inboundContext) {
+            return Model.of(inboundContext);
+        }
+
+        @Override
+        public void detach() {
         }
     }
 
@@ -159,19 +217,46 @@ public class ContextTreePanel extends Panel {
 
         private WebMarkupContainer listContainer;
 
-        private ListView<InboundContext> listView;
+        private DataView<InboundContext> dataView;
 
-        ListDragSource(WebMarkupContainer listContainer, ListView<InboundContext> listView) {
+        ListDragSource(WebMarkupContainer listContainer, DataView<InboundContext> dataView) {
             super(Operation.MOVE);
-            this.listView = listView;
+            this.dataView = dataView;
             this.listContainer = listContainer;
         }
 
         @Override
         public void onAfterDrop(AjaxRequestTarget target, Transfer transfer) {
             InboundContext inboundContext = transfer.getData();
-            listView.getModelObject().remove(inboundContext);
+            InboundContextDataProvider dataProvider = (InboundContextDataProvider) dataView.getDataProvider();
+            dataProvider.remove(inboundContext);
             target.add(listContainer);
+        }
+    }
+
+    private class ListDropTarget extends DropTarget {
+
+        private WebMarkupContainer listContainer;
+
+        private DataView<InboundContext> dataView;
+
+        ListDropTarget(WebMarkupContainer listContainer, DataView<InboundContext> dataView) {
+            super(Operation.MOVE);
+            this.dataView = dataView;
+            this.listContainer = listContainer;
+        }
+
+        @Override
+        public void onDrop(AjaxRequestTarget target, Transfer transfer, Location location) throws Reject {
+            InboundContext inboundContext = transfer.getData();
+            InboundContextDataProvider dataProvider = (InboundContextDataProvider) dataView.getDataProvider();
+
+            if (dataProvider.contains(inboundContext)) {
+                throw new Reject();
+            } else {
+                dataProvider.add(inboundContext);
+                target.add(listContainer);
+            }
         }
     }
 
