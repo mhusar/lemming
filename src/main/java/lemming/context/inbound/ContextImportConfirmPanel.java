18485ea4e284983ca718077621dd5d1ec12167a5
Marcus Husar <marcus.husar@gmail.com>
2019-07-24 13:17:54 +0200

    Add a modal panel to confirm import of contexts.

---
+++ src/main/java/lemming/context/inbound/ContextImportConfirmPanel.java
@@ -0,0 +1,38 @@
+package lemming.context.inbound;
+
+import lemming.ui.panel.ModalMessagePanel;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.model.StringResourceModel;
+
+public class ContextImportConfirmPanel extends ModalMessagePanel {
+
+    public ContextImportConfirmPanel() {
+        super("importConfirmPanel", DialogType.YES_NO);
+    }
+
+    @Override
+    protected String getTitleString() {
+        return getString("Action.reallyImport");
+    }
+
+    @Override
+    protected StringResourceModel getMessageModel() {
+        return new StringResourceModel("ContextImportConfirmPanel.message");
+    }
+
+    @Override
+    protected String getConfirmationString() {
+        return getString("Action.reallyImport");
+    }
+
+    @Override
+    protected void onCancel() {
+    }
+
+    @Override
+    protected void onConfirm(AjaxRequestTarget target) {
+        new InboundContextPackageDao().importContexts((InboundContextPackage) getDefaultModelObject());
+        InboundContextPackagePanel panel = (InboundContextPackagePanel) getPage().get("contextPackagePanel");
+        target.add(panel);
+    }
+}
